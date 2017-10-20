656e71053a09f320c6f3458807e0fc3cf9ecd02b
Sergey Bokhnyak <sergeybok@gmail.com>
2017-10-20 16:13:02 +0200

    Not working but am going to try something else

--- Assignment2/matrixmult/blocked_dgemm.c
+++ Assignment2/matrixmult/blocked_dgemm.c
@@ -8,6 +8,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 /* block parameter ... */
 #ifndef BLOCK_SIZE
@@ -16,13 +17,55 @@
 
 
 
+
+
+void square_naive (const double *A, const double *B,
+	      double *C,
+	      const unsigned dim, int s)
+{
+    unsigned  i, j, k;
+
+    for (i = 0; i < s; i++)
+    {
+    	const double *A_ix = A + i*dim;
+    	for (j = 0; j < s; j++)
+	    {
+            const double *B_xj = B + j;
+
+            double cij = C[i*dim + j];
+	        unsigned k_ind = 0;
+
+            for (k = 0; k < s; k++)
+	        {
+    	       cij += A_ix[k] * B_xj[k_ind];
+	           k_ind += dim;
+            }
+
+            C[i*dim + j] = cij;
+    	}
+    }
+}
+
+
 /**
  *  square_dgemm -- multiply two block matrices A and B adding result to C, result is C = C + A*B
  */
 void square_dgemm (const double  *A, const double  *B,  double  *C, const unsigned  M)
 {
-	/* TODO: implement the blocked matrix-matrix multiplication */
-        printf("implement square_dgemm routine\n");
-        exit(0);
+	//int n = 10; // Need n, where n is size of matrxi A: nxn, B: nxn, C:nxn
+	int s = (int) sqrt(BLOCK_SIZE/3.0);
+	for (int i =0; i < M/s; i++) {
+		for (int j =0; j < M/s; j++) {
+			double *C_ij = &C[i*s*M + j*s];
+			
+			for (int k =0; k < M/s; k++) {
+				const double *A_ik = &A[i*s*M + k*s];
+				const double *B_kj = &B[k*s*M + j*s];
+				square_naive(A_ik, B_kj, C_ij, M, s);
+			}
+		}
+	}
 }
+
+
 
